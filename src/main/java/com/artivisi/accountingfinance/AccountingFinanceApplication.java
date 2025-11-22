266dee8e143e87edd3e8146c925129ea95fd924c
Endy Muhardin <endy.muhardin@gmail.com>
2025-11-22 16:31:23 +0700

    starter spring 4 app

---
+++ src/main/java/com/artivisi/accountingfinance/AccountingFinanceApplication.java
@@ -0,0 +1,13 @@
+package com.artivisi.accountingfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingFinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountingFinanceApplication.class, args);
+	}
+
+}
